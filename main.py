887a64c5ba5a4b5ebfcc0412292119c59db3d42e
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2025-04-21 18:13:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 from flask import Flask,request
 
-import json
-from json import dumps
-from kafka import KafkaProducer
+
+from confluent_kafka import Producer
+import socket
+import random
 
 topic_name='captured_data'
 
-producer = KafkaProducer(
-    bootstrap_servers='{}',
-    sasl_mechanism='SCRAM-SHA-256',
-    security_protocol='SASL_SSL',
-    sasl_plain_username='{}',
-    sasl_plain_password='{}',
-    value_serializer=lambda x: dumps(x).encode('utf-8')
-)
+
+
+conf = {'bootstrap.servers': 'pkc-abcd85.us-west-2.aws.confluent.cloud:9092',
+        'security.protocol': 'SASL_SSL',
+        'sasl.mechanism': 'PLAIN',
+        'sasl.username': 'V6WGH4DZCKC2GVA2',
+        'sasl.password': 'c86O3OXHz07d2QppToyG0bdOBc7I5j3i3fUnvKAisEI6Gj6jZjgwCwj6er3f2jpT',
+        'client.id': socket.gethostname()}
+
+producer = Producer(conf)
 
 app=Flask(__name__)
 
@@ -27,7 +30,7 @@ def messagepasser():
         message=request.json
         print("Input Message : ",message)
         print("Sending kafka message")
-        resp = producer.send(topic_name, value=message)
+        resp = producer.produce(topic = topic_name, key=random.randint(0, 1000) value=message)
         print(resp)
         print("Sent message to kafka")
         return "Success"
